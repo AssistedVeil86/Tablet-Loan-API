362ee25d0206571ffdae29b07937824a171d6b5a
AssistedVeil86 <wilmerfernando2006@gmail.com>
2026-05-24 19:11:18 -0600

    Refacto KohaStudentService

--- TabletLoan.VSA/Infrastructure/Services/KohaStudentService.cs
+++ TabletLoan.VSA/Infrastructure/Services/KohaStudentService.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using ErrorOr;
 using Hangfire;
+using Refit;
 using TabletLoan.VSA.Infrastructure.ExternalAPIs.Koha;
 using TabletLoan.VSA.Infrastructure.ExternalAPIs.Koha.BackgroundJobs;
 using TabletLoan.VSA.Infrastructure.ExternalAPIs.Koha.DTOs.Request;
@@ -27,24 +29,34 @@ public sealed class KohaStudentService(
         }
 
         var kohaRequest = new KohaSearchPatronRequest(cif, activeCookie);
-        var kohaResponse = await kohaClient.SearchPatronAsync(kohaRequest);
 
-        if (!kohaResponse.Success)
+        try
         {
-            if (kohaResponse.Error == "Sesión expirada")
+            // Intentamos consumir la API
+            var kohaResponse = await kohaClient.SearchPatronAsync(kohaRequest);
+
+            // Si Koha devuelve 200 OK, pero dice Success = false en el JSON
+            if (!kohaResponse.Success)
             {
-                //Retry Koha Login
-                backgroundJobs.Enqueue<IRefreshKohaSessionJob>(
-                    job => job.ExecuteLoginAsync());
-                    
-                return KohaErrors.KohaSessionExpired("Koha's Session has expired");
+                return Error.Failure("Koha.ApiError", kohaResponse.Error ?? "Unknown Koha error");
             }
 
+            if (kohaResponse.Patrons.Count == 0)
+                return KohaErrors.KohaPatronNotFound($"Student with Id {cif} was not found.");
+
+            return kohaResponse.Patrons.First();
         }
-
-        if (kohaResponse.Patrons.Count == 0)
-            return KohaErrors.KohaPatronNotFound($"Student with Id {cif} was not found.");
-
-        return kohaResponse.Patrons.First();
+        catch (ApiException ex) when (ex.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            return KohaErrors.KohaSessionExpired("Koha's Session has expired (HTTP 401).");
+        }
+        catch (ApiException ex)
+        {
+            return Error.Failure("Koha.NetworkError", $"Koha API failed with status: {ex.StatusCode}");
+        }
+        catch (Exception ex)
+        {
+            return Error.Unexpected("Koha.Unexpected", ex.Message);
+        }
     }
 }
